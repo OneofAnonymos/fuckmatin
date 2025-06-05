a417a6af321d6641f1cf23731f23e94330181763
OneofAnonymos <165178069+OneofAnonymos@users.noreply.github.com>
2025-06-05 21:17:45 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+
+import os
 import time
 from telegram import Bot
 from telegram.ext import CommandHandler, Updater
 
-TOKEN = '7479003161:AAEnr-NQ-oYZ1_hc2AbTgZGzAanQmFNCoIg'
-CHANNEL_ID = '@fuck_MxATIn'  
+TOKEN = os.getenv("BOT_TOKEN", "7479003161:AAEnr-NQ-oYZ1_hc2AbTgZGzAanQmFNCoIg")
+CHANNEL_ID = os.getenv("CHANNEL_ID", "@Fuck_MxATiN")
 
 def spam(update, context):
     for i in range(258):
         context.bot.send_message(chat_id=CHANNEL_ID, text='@MxATiN')
-        time.sleep(0.5)  
+        time.sleep(0.5)
 
 updater = Updater(TOKEN)
 updater.dispatcher.add_handler(CommandHandler('spam', spam))
